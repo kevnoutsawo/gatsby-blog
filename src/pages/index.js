93d90d1ed22c784e23556b7b0da81b0ab6d2ffe6
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-09-24 18:08:37 +0100

    Updated Home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to my website</h1>
+    <p>This is a test site to get a firm grasp of Gatsby</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -19,10 +18,12 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
+    {/*
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
+    */}
   </Layout>
 )
 
