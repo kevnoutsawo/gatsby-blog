f1db40c30639fdf982e251f3ef87d65fc55f1344
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-09-24 17:56:46 +0100

    Updated title text and background

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `coral`,
       marginBottom: `1.45rem`,
     }}
   >
