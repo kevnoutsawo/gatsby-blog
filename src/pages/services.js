ded827072974a81b80ba654eec5d29b99e08056e
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-09-27 16:29:20 +0100

    Created Services page

---
+++ src/pages/services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ServicesPage = () => {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur est natus itaque ea accusantium consequuntur dolore laudantium veniam repellendus! Dignissimos consequuntur pariatur recusandae fugit numquam cupiditate, esse commodi repellendus impedit, voluptas aperiam alias officiis voluptate repudiandae? Eum, dolores enim.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur est natus itaque ea accusantium consequuntur dolore laudantium veniam repellendus! Dignissimos consequuntur pariatur recusandae fugit numquam cupiditate, esse commodi repellendus impedit, voluptas aperiam alias officiis voluptate repudiandae? Eum, dolores enim.</p>
+        </div>
+    )
+}
+
+export default ServicesPage;
