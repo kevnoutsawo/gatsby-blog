9d75a0ea2e7e8199f4f1e4bfc42a793b1e52ed95
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-09-27 16:27:36 +0100

    Created About page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>Welcome to my website</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur est natus itaque ea accusantium consequuntur dolore laudantium veniam repellendus! Dignissimos consequuntur pariatur recusandae fugit numquam cupiditate, esse commodi repellendus impedit, voluptas aperiam alias officiis voluptate repudiandae? Eum, dolores enim.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur est natus itaque ea accusantium consequuntur dolore laudantium veniam repellendus! Dignissimos consequuntur pariatur recusandae fugit numquam cupiditate, esse commodi repellendus impedit, voluptas aperiam alias officiis voluptate repudiandae? Eum, dolores enim.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
